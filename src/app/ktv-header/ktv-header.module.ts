411824dd144cb10d4c3c55892edd5e7849d48c4c
ervikasagarwal <vkasbnsl888@gmail.com>
2020-06-15 02:01:00 +0530

    +added icon injector +scoll styles +search, menu icon +color theme

--- src/app/ktv-header/ktv-header.module.ts
+++ src/app/ktv-header/ktv-header.module.ts
@@ -3,12 +3,14 @@ import { KridaTvHeaderComponent } from './ktv-header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { IconInjectorModule } from 'src/core/modules/icon-injector/app.module';
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
-        RouterModule
+        RouterModule,
+        IconInjectorModule
     ],
     declarations: [KridaTvHeaderComponent],
     exports: [KridaTvHeaderComponent],
